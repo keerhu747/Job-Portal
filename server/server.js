f017c482de44d4369d95dcb21cdb5abb534afe4d
keerthana <keerthanakeerthana70348@gmnail.com>
2026-02-10 12:32:23 +0530

    first commit

---
+++ server/server.js
@@ -0,0 +1,24 @@
+const express = require("express");
+const cors = require("cors");
+require("dotenv").config();
+
+const app = express();
+
+// middleware
+app.use(cors());
+app.use(express.json());
+
+// test route
+app.get("/", (req, res) => {
+  res.send("Job Portal Server Running ✅");
+});
+
+// routes
+// const userRoutes = require("./routes/userRoutes");
+// app.use("/api/users", userRoutes);
+
+const PORT = process.env.PORT || 5000;
+
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`);
+});
